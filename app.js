ec9a595f150c4d53420605a21c1e7c10d7051f71
Nicolai <nicolai@mmg.io>
2017-02-01 22:33:40 +0000

    Added final route code and header access

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 'use strict';
 
-var express = require('express');
+var express = require("express");
 var app = express();
-var routes = require('./routes');
+var routes = require("./routes");
 
-var jsonParser = require('body-parser').json;
+var jsonParser = require("body-parser").json;
 var logger = require("morgan");
 
-app.use(logger('dev'));
-
+app.use(logger("dev"));
 app.use(jsonParser());
+
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/qa");
+
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+ console.error("Connection Error", err);
+});
+
+db.once('open', function(){
+ console.log("DB Connection Successful");
+});
+
+app.use(function(req, res, next){
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ if(req.method === "OPTIONS") {
+  res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+  return res.status(200).json({});
+ }
+ next();
+});
 
 app.use("/questions", routes);
 
@@ -28,7 +51,7 @@ app.use(function(err, req, res, next){
   error: {
    message: err.message
   }
- })
+ });
 });
 
 var port = process.env.PORT || 3000;
